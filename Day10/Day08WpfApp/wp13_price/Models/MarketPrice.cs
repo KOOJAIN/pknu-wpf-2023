c1c26f336bd08e2b86129ca6fd58e578f7f63168
KOOJAIN <rnwkdls97@naver.com>
2023-05-01 17:30:26 +0900

    5.1

---
+++ Day10/Day08WpfApp/wp13_price/Models/MarketPrice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wp13_price.Models
+{
+    internal class MarketPrice
+    {
+        public string MidName { get; set; }
+        public string GoodName { get; set; }
+        public double Danq { get; set; }
+        public string Dan { get; set; }
+        public string Poj { get; set; }
+        public string SizeName { get; set; }
+        public string Lv { get; set; }
+        public int MinCost { get; set; }
+        public int MaxCost { get; set; }
+        public int AveCost { get; set; }
+        public DateTime Saledate { get; set; }
+        public string CmpName { get; set; }
+        public string LargeName { get; set; }
+    }
+}
